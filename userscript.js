ceff03bdafe7ef47df4f1f77f4b075285a0e610d
Thomas Wilhelm <quodos@users.noreply.github.com>
2018-11-19 15:10:55 +0100

    calculate the price without basic lands as well

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MKM wants list price calculator
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Calculates and prints the total value of a want list in cardmarket.com.
 // @author       Thomas Wilhelm
 // @include      https://www.cardmarket.com/en/Magic/MainPage/showWants*
@@ -13,16 +13,29 @@
     'use strict';
 
     const rows = document.querySelectorAll('table.MKMTable.wantsTable tbody tr');
+    // Ingore cards matching this regex
+    const ignore = /^(((Island)$|(Island \(Ver))|((Insel)$|(Insel \(Ver))|((Mountain)$|(Mountain \(Ver))|((Gebirge)$|(Gebirge \(Ver))|((Forest)$|(Forest \(Ver))|((Wald)$|(Wald \(Ver))|((Plains)$|(Plains \(Ver))|((Ebene)$|(Ebene \(Ver))|((Swamp)$|(Swamp \(Ver))|((Sumpf)$|(Sumpf \(Ver)))/g;
     let totalPrice = 0.0;
+    let totalPriceWithoutBasics = 0.0;
 
     Array.prototype.forEach.call(rows, function(el, i){
         const cells = el.children;
         const value = parseFloat(cells[12].querySelector('div').textContent.replace(',', '.'));
         totalPrice += value;
+
+        const cardName = cells[2].querySelector('a').textContent;
+        if (ignore.test(cardName) === false) {
+            totalPriceWithoutBasics += value;
+        }
     });
 
     const footer = document.querySelector('table.MKMTable.wantsTable tfoot tr td');
     footer.style.textAlign = 'right';
-    footer.innerHTML = `Gesamtpreis: <strong>&euro; ${totalPrice.toFixed(2)}</strong>`;
-    console.log(totalPrice);
+    if (totalPrice === totalPriceWithoutBasics) {
+        footer.innerHTML = `Gesamtpreis: <strong>&euro; ${totalPrice.toFixed(2)}</strong>`;
+    } else {
+        footer.innerHTML = `Gesamtpreis: <strong>&euro; ${totalPrice.toFixed(2)}</strong> / Ohne Standardländer: <strong>&euro; ${totalPriceWithoutBasics.toFixed(2)}</strong>`;
+    }
+
+    console.log(totalPrice, totalPriceWithoutBasics);
 })();
